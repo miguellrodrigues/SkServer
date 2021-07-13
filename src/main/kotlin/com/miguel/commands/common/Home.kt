dc09ff60523aea33d5a70906af0c9dbf8220d311
Miguel L. Rodrigues <miguellukas52@gmail.com>
2021-07-13 14:24:09 -0300

    banco transfer

--- src/main/kotlin/com/miguel/commands/common/Home.kt
+++ src/main/kotlin/com/miguel/commands/common/Home.kt
@@ -1,8 +1,10 @@
 package com.miguel.commands.common
 
+import com.miguel.game.bank.BankManager
 import com.miguel.game.home.HomeManager
 import com.miguel.game.manager.PlayerManager
 import com.miguel.values.Strings
+import com.miguel.values.Values
 import net.kyori.adventure.audience.MessageType
 import net.kyori.adventure.identity.Identity
 import net.kyori.adventure.text.Component
@@ -24,7 +26,7 @@ class Home : BukkitCommand("home") {
         }
 
         if (args.isEmpty()) {
-            sender.sendMessage("${Strings.PREFIX} Use §c/home [list] [set | go | delete] [nome]")
+            sender.sendMessage("${Strings.MESSAGE_PREFIX}Use §c/home [list] [set | go | delete] [nome]")
         } else if (args.size == 1) {
             if (args[0] == "list") {
                 val playerHomes = PlayerManager.getHomes(sender.uniqueId)
@@ -64,23 +66,33 @@ class Home : BukkitCommand("home") {
         } else if (args.size == 2) {
             when (args[0]) {
                 "set" -> {
-                    val message = HomeManager.setHome(sender, args[1])
+                    if (PlayerManager.getHomes(sender.uniqueId).size < Values.MAX_HOMES_PER_PLAYER && !sender.isOp) {
+                        val message = HomeManager.setHome(sender, args[1])
 
-                    sender.sendMessage(message)
+                        sender.sendMessage(message)
+                    } else {
+                        sender.sendMessage("${Strings.MESSAGE_PREFIX}Você atingiu o número máximo de homes §e!")
+                        sender.playSound(sender.location, Sound.BLOCK_NOTE_BLOCK_FLUTE, 1.0F, 1.0F)
+                    }
                 }
 
                 "go" -> {
-                    val home = HomeManager.getHome(sender, args[1])
+                    if (BankManager.withDraw(sender.uniqueId, 10.0)) {
+                        val home = HomeManager.getHome(sender, args[1])
 
-                    if (home == null) {
-                        sender.sendMessage("Home não encontrada !")
+                        if (home == null) {
+                            sender.sendMessage("Home não encontrada !")
+                        } else {
+                            sender.teleport(
+                                home.location.toBukkitLocation()
+                            )
+
+                            sender.sendMessage("§fTeleportado para a home §e${home.name.uppercase(Locale.getDefault())}")
+                            sender.playSound(sender.location, Sound.ENTITY_ENDERMAN_TELEPORT, 1.0F, 1.0F)
+                        }
                     } else {
-                        sender.teleport(
-                            home.location.toBukkitLocation()
-                        )
-
-                        sender.sendMessage("§fTeleportado para a home §e${home.name.uppercase(Locale.getDefault())}")
-                        sender.playSound(sender.location, Sound.ENTITY_ENDERMAN_TELEPORT, 1.0F, 1.0F)
+                        sender.sendMessage("${Strings.MESSAGE_PREFIX}Saldo insuficiente §e! §fVocê precisa ter ao menos §e10 §aUkranianinhos")
+                        sender.playSound(sender.location, Sound.BLOCK_NOTE_BLOCK_FLUTE, 1.0F, 1.0F)
                     }
                 }
 
@@ -93,7 +105,7 @@ class Home : BukkitCommand("home") {
                 }
             }
         } else {
-            sender.sendMessage("${Strings.PREFIX}Use §c/home [set | go | delete] [nome]")
+            sender.sendMessage("${Strings.MESSAGE_PREFIX}Use §c/home [set | go | delete] [nome]")
         }
 
         return false
--- src/main/kotlin/com/miguel/values/Values.kt
+++ src/main/kotlin/com/miguel/values/Values.kt
@@ -2,6 +2,7 @@ package com.miguel.values
 
 object Values {
 
+    const val MAX_HOMES_PER_PLAYER = 10
     var DAMAGE = true
     var CHAT = true
 }
